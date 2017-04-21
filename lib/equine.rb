4afda74c64c8cd9b43f3d461fad9979b554a531c
“sstone72389” <sstone72389@gmail.com>
2017-04-21 12:21:21 -0400

    Completes Ruby Mixin Training
    Completes required training

--- lib/equine.rb
+++ lib/equine.rb
@@ -1,11 +1,28 @@
+# silly linter
+module Ridable
+  def can_i_ride
+    puts 'yes I am Ridable'
+  end
+end
+# silly linter
 class Equine
-end
+  def initialize
+    @num_legs = 4
+  end
 
-class Horse
-end
-
-class Mule
-end
-
-class Zebra
+  def eat_grass
+    puts 'yummmmmm grass is delish bro'
+  end
+  # silly linter
+  class Horse < Equine
+    inlude Ridable
+  end
+  # silly linter
+  class Mule < Equine
+    inlude Ridable
+  end
+  # silly linter
+  class Zebra < Equine
+    inlude Ridable
+  end
 end
